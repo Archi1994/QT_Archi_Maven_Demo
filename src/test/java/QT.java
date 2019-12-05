0658f616088c02185f7075ef7501530a404a8876
Archi1994 <archanataneja17@gmail.com>
2019-12-05 10:38:44 +0530

    added testng

--- src/test/java/QT.java
+++ src/test/java/QT.java
@@ -3,8 +3,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.Test;
 
 public class QT {
+	
+	@Test
 	public static void main(String[]args) throws InterruptedException
     {
            //String key= "webdriver.chrome.driver"; 
